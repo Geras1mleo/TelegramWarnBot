4b900d6ae2363d966c9af2ea0d9019380a548fe9
geras1mleo <svyatoslav30@ukr.net>
2022-07-21 21:48:48 +0200

    Tests project added. TriggersHandler testing with Moq package

---
+++ TelegramWarnBot.Tests/Usings.cs
@@ -0,0 +1,11 @@
+global using TelegramWarnBot.Tests;
+
+global using Xunit;
+global using Moq;
+global using TelegramWarnBot;
+
+global using Telegram;
+global using Telegram.Bot;
+global using Telegram.Bot.Polling;
+global using Telegram.Bot.Types;
+global using Telegram.Bot.Types.Enums;
